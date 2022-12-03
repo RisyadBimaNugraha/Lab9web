c14d353ed61c33ce9faf300a7a51dde600408bcc
Risyad Bima Nugraha <risyadbimanugraha@gmail.com>
2022-12-04 05:03:14 +0700

    menambahkan file lab9_php_modular

---
+++ lab9_php_modular/home.php
@@ -0,0 +1,17 @@
+<?php require('header.php'); ?>
+
+<div class="content">
+    <h2>Ini Halaman Home</h2>
+    <p>Ini adalah bagian content dari halaman.</p>
+</div>
+
+<?php require('footer.php'); ?>
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact
