4275bfc8f86c769f755d2163a5caaa8cb61de6a9
Risyad Bima Nugraha <risyadbimanugraha@gmail.com>
2022-12-04 05:04:18 +0700

    menambahkan file lab9_php_tugas

---
+++ lab9_php_tugas/footer.php
@@ -0,0 +1,11 @@
+  <footer>
+            <p>&copy; 2022, Ananda Fachri Reynaldi, Universitas Pelita Bangsa</p>
+        </footer>
+    </div>
+</body>
+</html>
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
